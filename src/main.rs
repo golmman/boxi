95736adcd9f93f7a9fd40cb91da1e780297dea59
Dirk Kretschmann <kretschmanndi@gmail.com>
2025-03-30 22:11:23 +0200

    add basic graph drawer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let graph_drawer = GraphDrawer::new();
+    graph_drawer.draw();
+}
+
+struct Node {
+    text: String,
+}
+
+struct GraphDrawerBuffer {
+    buffer: String,
+}
+
+struct GraphDrawer {}
+
+impl GraphDrawer {
+    fn new() -> Self {
+        Self {}
+    }
+
+    pub fn draw(&self) {
+        let mut buffer = GraphDrawerBuffer {
+            buffer: String::from(""),
+        };
+        buffer.buffer.push_str("hello");
+        println!("{}", buffer.buffer);
+    }
+
+    fn draw_box(&self, node: Node) {}
 }
